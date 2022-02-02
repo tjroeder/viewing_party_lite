dd361f5ccc790539862ed3eb1e38e290c67b352e
Kelly Anderson <kean9528@colorado.edu>
2022-02-01 22:12:37 -0700

    outline form

---
+++ spec/features/parties/new_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'New party' do
+
+  describe 'view' do
+    it 'I see a form to register a new user' do
+      visit ""
+      fill_in ""
+    end
+  end
+end
