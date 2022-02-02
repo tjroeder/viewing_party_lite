8460afaa27bb755c733ad09e276da2035656e431
Kelly Anderson <kean9528@colorado.edu>
2022-02-02 11:04:29 -0700

    fix check boxes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get '/register', to: 'users#new'
 
+
   resources :users, only: %i[show create] do
     get '/discover', to: 'users#discover'
     resources :movies, only: %i[index show] do
