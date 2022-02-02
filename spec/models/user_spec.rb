f5c02a6352ebf8fe91ca972a8c3d02b89528826e
Tim Roeder <timothyjroeder@gmail.com>
2022-02-01 20:05:58 -0700

    Test: add valid email uniqueness check

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe User, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:name) }
   end
 
