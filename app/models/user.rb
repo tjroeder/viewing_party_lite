6a565d4760b51b99f22c6cd4a6977489aca47c41
Tim Roeder <timothyjroeder@gmail.com>
2022-02-02 14:23:46 -0700

    Fix: remove email validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :user_parties, dependent: nil
   has_many :parties, through: :user_parties
 
-  validates :email, presence: true, uniqueness: true, 'valid_email_2/email': true
+  validates :email, presence: true, uniqueness: true
   validates :name, presence: true
 end
