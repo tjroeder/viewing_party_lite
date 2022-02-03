e20434af26580742608389ddbea2397238d3d225
Tim Roeder <timothyjroeder@gmail.com>
2022-02-02 22:16:14 -0700

    Feat: create ::get_reviews, and ::conn

--- app/services/review_service.rb
+++ app/services/review_service.rb
@@ -1,12 +1,10 @@
 class ReviewService
   def self.get_reviews(movie_id)
-    response = conn(movie_id).get()
-    json = JSON.parse(response.body, symbolize_names: true)
-    require 'pry'; binding.pry
+    response = conn(movie_id).get
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.conn(movie_id)
-
     url = "https://api.themoviedb.org/3/movie/#{movie_id}/reviews"
 
     Faraday.new(url: url) do |f|
