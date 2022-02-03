e466fb250d851f72ad9ca2b8233c3093a32c25e5
Tim Roeder <timothyjroeder@gmail.com>
2022-02-03 13:31:02 -0700

    Feat: add ::top_rated_movies service, change ::conn method

---
+++ app/services/movie_service.rb
@@ -0,0 +1,17 @@
+class MovieService
+  def self.movie(movie_id)
+    response = conn.get("movie/#{movie_id}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+  
+  def self.top_rated_movies
+    response = conn.get('movie/top_rated')
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    url = 'https://api.themoviedb.org/3/'
+
+    Faraday.new(url: url, params: { api_key: ENV['tmdb_api_key'] })
+  end
+end
