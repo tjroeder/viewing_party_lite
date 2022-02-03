8661bd6ca07409f8bd6452e504c86149f8ae0937
Tim Roeder <timothyjroeder@gmail.com>
2022-02-02 22:15:35 -0700

    Feat: create ::movie_reviews

--- app/facades/movies_facade.rb
+++ app/facades/movies_facade.rb
@@ -1,5 +1,13 @@
 class MoviesFacade
-  def self.movies
-    
+  def self.movie_reviews(movie_id)
+    json = ReviewService.get_reviews(movie_id)
+
+    json[:results].map do |review_data|
+      Review.new(review_data)
+    end
   end
+
+  # def self.movie_list
+
+  # end
 end
