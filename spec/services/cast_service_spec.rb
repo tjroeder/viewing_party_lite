84bf50767aeff11ffb107fabcb534184c8b5e63b
Tim Roeder <timothyjroeder@gmail.com>
2022-02-03 14:11:00 -0700

    Feat: updated to match movie_service methods

--- spec/services/cast_service_spec.rb
+++ spec/services/cast_service_spec.rb
@@ -3,10 +3,19 @@ require 'rails_helper'
 RSpec.describe CastService, type: :service do
   context 'class methods' do
     describe '::conn', :vcr do
-      it 'returns casts' do
-        casts = CastService.get_cast(11)
-        expect(casts).to be_a(Hash)
-        expect(casts[:cast]).to be_a(Array)
+      it 'returns Faraday connection' do
+        cast = CastService.conn
+
+        expect(cast).to be_a(Faraday::Connection)
+      end
+    end
+
+    describe '::get_cast', :vcr do
+      it 'returns cast data' do
+        cast = CastService.get_cast(11)
+
+        expect(cast).to be_a(Hash)
+        expect(cast[:cast]).to be_a(Array)
       end
     end
   end
